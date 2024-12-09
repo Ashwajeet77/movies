18202e6582010cf39aa6e83ee447c42c0da0e6b1
Ashwajeet77 <ashwajeetgupta18@gmail.com>
2024-12-09 20:57:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/kQcX1LL/movie-time-neon-sign-sign-24908-55555.jpg')).split()
-FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
+FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/bXtGwn4/premium-2x-min.png')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/_kA.jpg')).split()
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
